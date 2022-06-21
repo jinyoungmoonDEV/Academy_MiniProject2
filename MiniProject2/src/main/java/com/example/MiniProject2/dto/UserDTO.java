979a0f3d24f779ba82562b74f1083caf50d3cf97
Moon <moonjinyoung.dev@gmail.com>
2022-06-21 22:38:02 +0900

    Update

--- MiniProject2/src/main/java/com/example/MiniProject2/dto/UserDTO.java
+++ MiniProject2/src/main/java/com/example/MiniProject2/dto/UserDTO.java
@@ -1,14 +1,12 @@
 package com.example.MiniProject2.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@RequiredArgsConstructor
 public class UserDTO {
     private String phone_number;
     private Long address;
--- MiniProject2/src/main/java/com/example/MiniProject2/service/UserService.java
+++ MiniProject2/src/main/java/com/example/MiniProject2/service/UserService.java
@@ -1,20 +1,13 @@
 package com.example.MiniProject2.service;
 
-import com.example.MiniProject2.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
+
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public interface UserService {
-    @Autowired
-    private final UserRepository userRepository;
 
-    public boolean login(User user){
-        User finduser = userRepository.findById(us)
-    }
 }
